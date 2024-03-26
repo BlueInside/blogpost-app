c035cb262b8699ab28dac0832a5ac4824a78b9b8
Karol <kpulawski13@gmail.com>
2024-03-26 11:23:26 +0000

    Connect to database

---
+++ config/database.js
@@ -0,0 +1,22 @@
+// Requires
+const debug = require('debug')('blogpost-app:database');
+const mongoose = require('mongoose');
+
+// Require configuration variables
+require('dotenv').config();
+
+// Establish connection with database
+const connect = async () => {
+  try {
+    if (!process.env.MONGODB_URL) {
+      throw new Error('MONGODB_URL is not defined');
+    }
+    await mongoose.connect(process.env.MONGODB_URL);
+    debug('Connected to the database');
+  } catch (err) {
+    debug('Error connecting to the database', err.message);
+    throw new Error('Failed to connect to the database');
+  }
+};
+
+module.exports = connect;

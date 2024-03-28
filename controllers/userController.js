49f708600bb3d62d0c5537beee9b49d5d65ad54b
Karol <kpulawski13@gmail.com>
2024-03-28 20:08:26 +0000

    Implement user_login controller for POST request

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,10 @@ const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+// Require dotenv
+require('dotenv').config();
 
 // Display list of all users.
 exports.user_list = asyncHandler(async (req, res, next) => {
@@ -13,7 +17,7 @@ exports.user_list = asyncHandler(async (req, res, next) => {
 // Create a new user
 exports.user_create = [
   // Run validation and sanitization
-  body('username')
+  body('username') // Checks username
     .trim()
     .notEmpty()
     .withMessage('Username field cannot be empty')
@@ -25,19 +29,19 @@ exports.user_create = [
       }
     })
     .escape(),
-  body('password')
+  body('password') // Checks password
     .trim()
     .notEmpty()
     .withMessage('Password field cannot be empty')
     .isLength({ min: 5 })
     .withMessage('Password must be at least 5 characters long')
     .escape(),
-  body('firstName')
+  body('firstName') // Checks firstName field
     .trim()
     .notEmpty()
     .withMessage('First name field cannot be empty')
     .escape(),
-  body('lastName')
+  body('lastName') // Checks lastName field
     .trim()
     .notEmpty()
     .withMessage('Last name field cannot be empty')
@@ -70,9 +74,35 @@ exports.user_create = [
 ];
 
 // Handle user login on POST
-exports.user_login = asyncHandler(async (req, res, next) => {
-  res.json({ message: 'Implement user login' });
-});
+exports.user_login = [
+  body('username').trim().escape(),
+  body('password').trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    // Find username in Database
+    const user = await User.findOne({ username: req.body.username });
+
+    // Checks if username is valid
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid username or password.' }); // Username doesn't match send error
+    }
+
+    // Checks if password match
+    const match = await bcrypt.compare(req.body.password, user.password);
+
+    // If password doesn't match send error
+    if (!match) {
+      return res.status(400).json({ error: 'Invalid username or password.' });
+    }
+
+    // Sign with jwt when user was found and password match
+    jwt.sign({ user }, process.env.JWT_SECRET_KEY, (err, token) => {
+      res.json({
+        token,
+      });
+    });
+  }),
+];
 
 // Display detail for a specific user
 exports.user_detail = (req, res, next) => {

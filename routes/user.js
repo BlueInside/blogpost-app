4afbadf3f27fe9313894d991421e91d40f62a25f
Karol <kpulawski13@gmail.com>
2024-03-29 20:50:10 +0000

    Add authorization to specific routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,11 @@
 // Require express
 const express = require('express');
+
 // Creates router
 const router = express.Router();
+
+// Requires functions to verifyToken and jwtVerify
+const { verifyToken, jwtVerify } = require('../lib/verifyToken.js');
 
 // Require controller modules
 const user_controller = require('../controllers/userController.js');
@@ -9,19 +13,19 @@ const user_controller = require('../controllers/userController.js');
 /// User ROUTES ///
 
 // GET request for getting all the users requires JWT TOKEN
-router.get('/', user_controller.user_list);
+router.get('/', verifyToken, jwtVerify, user_controller.user_list);
 
 // POST request for creating new user
 router.post('/', user_controller.user_create);
 
 // GET request to get user info requires JWT TOKEN
-router.get('/:id', user_controller.user_detail);
+router.get('/:id', verifyToken, jwtVerify, user_controller.user_detail);
 
 // PUT request for updating a user requires JWT TOKEN and uses id from it to update user
-router.put('/:id', user_controller.user_update);
+router.put('/:id', verifyToken, jwtVerify, user_controller.user_update);
 
 // DELETE request for deleting a user requires JWT TOKEN
-router.delete('/:id', user_controller.user_delete);
+router.delete('/:id', verifyToken, jwtVerify, user_controller.user_delete);
 
 // POST request for logging a user
 router.post('/login', user_controller.user_login);

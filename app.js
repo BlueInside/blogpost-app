6ccb8fb61d42ad698d37188560109480d2728432
Karol <kpulawski13@gmail.com>
2024-03-31 21:20:16 +0100

    Use cors middleware in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 // Create secret key for JWT
 require('./config/generateSecretKey');
 
@@ -24,6 +25,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
